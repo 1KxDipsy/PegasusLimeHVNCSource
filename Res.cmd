b7e7213846995966ccfb616dd9669696357c5628
Grim1312 <111816007+Grim1312@users.noreply.github.com>
2022-08-25 13:49:34 +0200

    Update Res.cmd

--- Res.cmd
+++ Res.cmd
@@ -1,6 +1,6 @@
 @echo off
 pushd "%~dp0"
-powershell Expand-7Zip -ArchiveFileName "Source\PEGASUS_LIME\Properties\Resources.7z.001" -TargetPath "Source\PEGASUS_LIME\Properties\"
+powershell Expand-7Zip -ArchiveFileName "Source\\PEGASUS_LIME\\Properties\\Resources.7z.001" -TargetPath "Source\\PEGASUS_LIME\\Properties\\"
 :exit
 popd
 @echo on
