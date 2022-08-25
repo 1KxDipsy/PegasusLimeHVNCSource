b008f5837f939a45953139ea8b953384252701bb
Grim1312 <111816007+Grim1312@users.noreply.github.com>
2022-08-25 14:04:19 +0200

    Update Build.cmd

--- Build.cmd
+++ Build.cmd
@@ -1,6 +1,6 @@
 @echo off
 pushd "%~dp0"
-powershell Compress-7Zip "Source\bin\Release" -ArchiveFileName "PEGASUSFULLVERSION.zip" -Format Zip
+powershell Compress-7Zip "Source\bin\Release" -ArchiveFileName "PEGASUSLIMEHVNC.zip" -Format Zip
 :exit
 popd
 @echo on
