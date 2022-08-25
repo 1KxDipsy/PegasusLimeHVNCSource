6b273cbc4bcdeb9c5bafad84a96c9e0c5b5141ad
Grim1312 <111816007+Grim1312@users.noreply.github.com>
2022-08-25 05:26:09 +0200

    Update Build.cmd

--- Build.cmd
+++ Build.cmd
@@ -1,6 +1,6 @@
 @echo off
 pushd "%~dp0"
-powershell Compress-7Zip "Update\bin\Release\net472" -ArchiveFileName "PEGASUSFULLVERSION.zip" -Format Zip
+powershell Compress-7Zip "Source\bin\Release" -ArchiveFileName "PEGASUSFULLVERSION.zip" -Format Zip
 :exit
 popd
 @echo on
